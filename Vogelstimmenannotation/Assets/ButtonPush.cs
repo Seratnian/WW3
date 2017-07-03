8b8657e259edceb31cd5452f12982e79daa751d2
Seratnian <sschleier@posteo.de>
2017-07-03 12:18:31 +0200

    Szenen mit VR überarbeitet

---
+++ Vogelstimmenannotation/Assets/ButtonPush.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPush : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedObj;
+    private GameObject collidingObject;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    public bool IsTriggerPressed = false;
+    public bool IsGrabPressed = false;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Update()
+    {
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            IsTriggerPressed = true;
+        }
+
+        if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            IsTriggerPressed = false;
+        }
+
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            IsGrabPressed = true;
+        }
+
+        if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+        {
+            IsGrabPressed = false;
+        }
+    }
+}
--- Vogelstimmenannotation/Assets/Scripts/ButtonPositionReset.cs
+++ Vogelstimmenannotation/Assets/Scripts/ButtonPositionReset.cs
@@ -5,18 +5,32 @@ public class ButtonPositionReset : MonoBehaviour
 {
     private Vector3 originalPosition;
     new private Rigidbody rigidbody;
-    private int force = 100;
+    private int force;
+    private int forceToStop;
+    private bool isInContact = false;
 
-	void Start ()
+    void Start()
     {
+        force = 100;
+        forceToStop = force * force;
         originalPosition = transform.position;
         rigidbody = GetComponent<Rigidbody>();
-	}
-	
-	void Update ()
+    }
+
+    void Update()
     {
-        Stop(true);
-	}
+        if (!isInContact)
+            Stop(true);
+        else
+        {
+            Vector3 direction = originalPosition - transform.position;
+            if (direction.z > 0)
+            {
+                rigidbody.AddForce(direction * forceToStop);  
+            }
+        }
+            
+    }
 
     void Stop(bool addForce = false)
     {
@@ -26,7 +40,32 @@ public class ButtonPositionReset : MonoBehaviour
         if (addForce)
         {
             Vector3 direction = originalPosition - transform.position;
+            Debug.Log(direction);
             rigidbody.AddForce(direction * force);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.GetComponent<ButtonPush>() != null)
+        {
+            isInContact = true;
+            ButtonPush buttonPusher = collider.GetComponent<ButtonPush>();
+            SteamVR_TrackedObject trackedObj = collider.GetComponent<SteamVR_TrackedObject>();
+            SteamVR_Controller.Device Controller = SteamVR_Controller.Input((int)trackedObj.index);
+            if (buttonPusher.IsTriggerPressed)
+            {
+                Vector3 direction = Controller.velocity;
+                rigidbody.AddForce(direction * force); 
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.GetComponent<ButtonPush>() != null)
+        {
+            isInContact = false;
         }
     }
 }
