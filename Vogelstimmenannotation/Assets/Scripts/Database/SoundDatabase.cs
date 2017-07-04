6cd64b8f898582ec2fa257f3f299eab572bbb84a
Unknown <chanawin90@gmail.com>
2017-07-04 09:56:08 +0200

    Bird spawning/despawning

---
+++ Vogelstimmenannotation/Assets/Scripts/Database/SoundDatabase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using WW3.Database;
+
+namespace WW3.TestScripts
+{
+    [Serializable]
+    public class SoundDatabase : MonoBehaviour, SoundDatabaseHandler
+    {
+        public string[] Url;
+
+        public static SoundDatabaseHandler SoundDatabaseHandler { private set; get; }
+
+        public string GetAudioClipUrl()
+        {
+            return Url[UnityEngine.Random.Range(0,Url.Length)];
+        }
+
+        public void IdentifyAudioclip(WWW clipUrl, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Awake()
+        {
+            SoundDatabaseHandler = this;
+        }
+    }
+}
