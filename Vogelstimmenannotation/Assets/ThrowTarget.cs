ea01660a20338cf34a64ab9682d7623bfda368b6
Seratnian <sschleier@posteo.de>
2017-07-14 16:51:48 +0200

    Vogel eingesetzt und Videos aufgenommen.

---
+++ Vogelstimmenannotation/Assets/ThrowTarget.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowTarget : MonoBehaviour {
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CanBeThrown ball = collision.gameObject.GetComponent<CanBeThrown>();
+        Debug.Log(ball);
+        Debug.Log(!ball);
+        if (ball)
+        {
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
